7c798608db66349567c76857f18af68adfe0713c
Fengye <124170659+AIRADSK@users.noreply.github.com>
2024-12-20 14:48:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -218,7 +218,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "枫叶", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -251,9 +251,12 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
-
+	sus = append(sus, 3482668189) //AIRA DSK
+	sus = append(sus, 3582687772) //
+	sus = append(sus, 1376968126) //
+	sus = append(sus, 1792157206) //乐崽
+	//sus = append(sus, 1480459201) //淼
+	
 	// 启用 webui
 	// go webctrl.RunGui(*g)
 
@@ -280,7 +283,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "枫叶", "亚托莉", "アトリ"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
