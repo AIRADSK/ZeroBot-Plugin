e504f7d5b3262812c684bf8f41315a37d027278f
AIRADSK <1948064956@qq.com>
2024-12-21 23:19:57 +0800

    1

--- plugin/givemoney/givemoney.go
+++ plugin/givemoney/givemoney.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/FloatTech/AnimeAPI/wallet"
-	//"github.com/FloatTech/floatbox/math"
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
 	zero "github.com/wdvxdr1123/ZeroBot"
@@ -99,66 +98,4 @@ func init() {
 				return
 			}
 		})
-	// engine.OnRegex(`给予\s(\d+)\s(\d+)`, zero.OnlyGroup).SetBlock(true).
-	// 	Handle(func(ctx *zero.Ctx) {
-	// 		nickname = ctx.GetThisGroupMemberInfo(
-	// 			math.Str2Int64(ctx.State["regex_matched"].([]string)[1]),
-	// 			false,
-	// 		).Get("nickname").Str
-	// 		if nickname == "" {
-	// 			nickname = ctx.GetStrangerInfo(
-	// 				math.Str2Int64(ctx.State["regex_matched"].([]string)[1]),
-	// 				false,
-	// 			).Get("nickname").Str
-
-	// 		}
-
-	// 		omy := math.Str2Int64(ctx.State["regex_matched"].([]string)[2])
-	// 		var (
-	// 			money = int(omy)
-
-	// 			ouid  = ctx.Event.UserID
-	// 			uidre = math.Str2Int64(ctx.State["regex_matched"].([]string)[1])
-	// 		)
-	// 		switch {
-	// 		case omy > 1000:
-	// 			ctx.SendChain(message.At(uidgi), message.Text("一次最多给予1000ATRI币哦~"))
-	// 			return
-	// 		case omy <= 0:
-	// 			ctx.SendChain(message.At(uidgi), message.Text("ATRI币数额必须大于0哦~"))
-	// 			return
-	// 		case 0 < omy && omy <= 1000:
-
-	// 			if uidInt != 0 {
-	// 				uidgi = uidInt
-	// 			} else {
-	// 				uidgi = ouid
-
-	// 			}
-	// 			if wallet.GetWalletOf(uidgi) < money {
-	// 				ctx.SendChain(message.Text("ATRI币不足,发送签到获取吧~"))
-	// 				return
-	// 			}
-	// 			err := wallet.InsertWalletOf(uidgi, -money)
-	// 			if err != nil {
-	// 				ctx.SendChain(message.Text("ATRI币扣除失败  [ERROR at gm.go:51]:", err))
-	// 				return
-	// 			}
-	// 			err1 := wallet.InsertWalletOf(uidre, money)
-	// 			if err1 != nil {
-	// 				ctx.SendChain(message.Text("接受ATRI币出错啦,返回扣除的ATRI币  [ERROR at gm.go:55]:", err1))
-	// 				err := wallet.InsertWalletOf(uidgi, money)
-	// 				if err != nil {
-	// 					ctx.SendChain(message.Text("返回ATRI币失败,您的钱被吞掉了~  [ERROR at gm.go:58]:", err))
-	// 					return
-	// 				}
-	// 			}
-	// 			ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(nickname+"接受了你的"+ctx.State["regex_matched"].([]string)[2]+"ATRI币"))
-	// 			return
-	// 		default:
-	// 			ctx.SendChain(message.Text("发生未知错误"))
-	// 			return
-	// 		}
-
-	// 	})
 }
